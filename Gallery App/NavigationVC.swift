4641c49e2f585d8b69de98728736a258d407e951
Rahul <rahulsaps099@gmail.com>
2017-08-19 21:13:21 +0530

    Project done
    Done

--- Gallery App/NavigationVC.swift
+++ Gallery App/NavigationVC.swift
@@ -16,6 +16,20 @@ class NavigationVC: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func retrieveDataButtonTapped(_ sender: UIButton) {
+        var alertMsg: String?
+        if let retrievedUsername = KeychainWrapper.standard.string(forKey: "username") ,let retrievedPassword = KeychainWrapper.standard.string(forKey: "password") {
+            alertMsg = "Retrieved Username is: \(retrievedUsername) \n Retrieved password is: \(retrievedPassword)"
+        }
+        else{
+            alertMsg = "Oops! Got nothing. Please save data in keychain."
+        }
+        let alert = UIAlertController(title: "Keychain Data", message: alertMsg, preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
--- Gallery App/SaveDataInKeychainVC.swift
+++ Gallery App/SaveDataInKeychainVC.swift
@@ -22,26 +22,14 @@ class SaveDataInKeychainVC: UIViewController {
     
 
     @IBAction func saveDataInKeychain(_ sender: UIButton) {
-        
         if let usrname = usernameTF.text, let pswd = passwordTF.text {
             let saveUsername: Bool = KeychainWrapper.standard.set(usrname, forKey: "username")
             let savepassword: Bool = KeychainWrapper.standard.set(pswd, forKey: "password")
             print("Save was successful: \(saveUsername), \(savepassword)")
+            self.dismiss(animated: true, completion: nil)
         }
     }
     
-    @IBAction func retrievePasswordButtonTapped(_ sender: UIButton) {
-        var alertMsg: String?
-        if let retrievedUsername = KeychainWrapper.standard.string(forKey: "username") ,let retrievedPassword = KeychainWrapper.standard.string(forKey: "password") {
-            alertMsg = "Retrieved Username is: \(retrievedUsername) \n Retrieved password is: \(retrievedPassword)"
-        }
-        else{
-            alertMsg = "Oops! Got nothing. Please save data in keychain."
-        }
-        let alert = UIAlertController(title: "Keychain Data", message: alertMsg, preferredStyle: UIAlertControllerStyle.alert)
-        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
-        self.present(alert, animated: true, completion: nil)
-    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
